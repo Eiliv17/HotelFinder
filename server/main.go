3bb2f3f7d5c27d05409efcf006bed65655c31339
Alberto Fabro <26629510+Eiliv17@users.noreply.github.com>
2023-02-28 17:42:15 +0100

    feat: add basic routes for search and hotels api

--- server/main.go
+++ server/main.go
@@ -1,13 +1,55 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/Eiliv17/HotelFinder/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.LoadDatabase()
+}
 
 func main() {
+
+	// setting up for production
+	if os.Getenv("GO_ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// creating the gin engine and setting up configurations
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.SetTrustedProxies(nil)
+
+	api := r.Group("/v1")
+	{
+		// search for nearby hotels endpoint
+		api.GET("/search/nearby")
+
+		// hotels endpoints
+		hotels := api.Group("/hotels")
+		{
+			// return detailed info about an hotel
+			hotels.GET("/:id")
+
+			// add an hotel
+			hotels.POST("")
+
+			// update details of an hotel
+			hotels.PUT("/:id")
+
+			// delete an hotel
+			hotels.DELETE("/:id")
+		}
+	}
+
+	// running the gin engine
+	if os.Getenv("GO_ENV") != "production" {
+		r.Run()
+	} else {
+		port := os.Getenv("PORT")
+		r.RunTLS(":"+port, "servercert.pem", "serverkey.key")
+	}
 }
